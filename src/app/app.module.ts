8cfa720435afdf4e47d6a8a1b158b13ec65babb4
Bhuvanagiri <2166191@cognizant.com>
2023-03-19 15:47:02 +0530

    initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { FooterComponent } from './Footer/footer/footer.component';
+import { HomeComponent } from './Main/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
